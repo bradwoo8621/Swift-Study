8bbd74f07483667c7f77c316fce1e122f7f2ef51
bradwoo8621 <bradwoo8621@gmail.com>
2017-04-07 22:44:16 +0800

    initialize

---
+++ Instagram/Instagram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by brad.wu on 2017/4/7.
+//  Copyright © 2017年 bradwoo8621. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
