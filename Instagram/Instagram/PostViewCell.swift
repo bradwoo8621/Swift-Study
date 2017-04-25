aa9ff53164c437a3e23e387ec62257da7822ea4d
bradwoo8621 <bradwoo8621@gmail.com>
2017-04-25 12:12:53 +0800

    some changes

---
+++ Instagram/Instagram/PostViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostViewCell.swift
+//  Instagram
+//
+//  Created by brad.wu on 2017/4/25.
+//  Copyright © 2017年 bradwoo8621. All rights reserved.
+//
+
+import UIKit
+
+class PostViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
